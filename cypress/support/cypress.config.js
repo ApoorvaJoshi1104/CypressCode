fa2fd33b2c7c53c6cf47b954c41abef7d00a2b5a
apoorva.joshi <apoorva.joshi@cimet.com.au>
2025-01-17 13:32:17 +0530

    adding.js file

---
+++ cypress/support/cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('/Users/apoorvajoshi/node_modules/cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
+});
