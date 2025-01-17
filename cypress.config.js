49721c9d14c232714be0bbc4835a5bc87704f716
apoorva.joshi <apoorva.joshi@cimet.com.au>
2025-01-17 16:07:21 +0530

    update

--- cypress.config.js
+++ cypress.config.js
@@ -4,15 +4,8 @@ module.exports = defineConfig({
   projectId: 'jghwbq',
   e2e: {
     setupNodeEvents(on, config) {
-      require('/Users/apoorvajoshi/node_modules/cypress-mochawesome-reporter/plugin')(on);
       return config;
-    },
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: false,
-      json: true,
+    
     },
   },
 });
